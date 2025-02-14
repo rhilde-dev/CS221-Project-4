fcc1fc8a7aa9d4223b5da4650f46eefab079bba9
rhilde-dev <ryleehilde@u.boisestate.edu>
2025-02-14 10:42:18 -0700

    worst case done,  just gottta finish

---
+++ MethodDriver.java
@@ -0,0 +1,17 @@
+
+/**
+ * Driver class to analyze methods
+ * 
+ * @author rhilde-dev
+ */
+public class MethodDriver {
+    public static void main(String[] args) {
+        
+        int[] array = {4,3,2,1};
+
+        MethodsToAnalyze methods = new MethodsToAnalyze();
+
+        methods.sortIt(array);
+
+    }
+}
